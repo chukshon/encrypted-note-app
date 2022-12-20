5375e34fac93cfb39408283586211f9a90977635
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-12-20 18:49:18 +0100

    feat: tip tap editor setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,49 @@ import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 
 function App() {
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: '<p>Hello World!</p>',
+    editorProps: {
+      attributes: {
+        class: styles.textEditor,
+      },
+    },
+  })
+
+  const toggleBold = () => {
+    editor?.chain().focus().toggleBold().run()
+  }
+  const toggleItalic = () => {
+    editor?.chain().focus().toggleItalic().run()
+  }
   return (
     <div className={styles.pageContainer}>
       <div className={styles.sidebar}>Sidebar</div>
       <div className={styles.editorContainer}>
-        <div className={styles.toolbar}>Toolbar</div>
-        Text Editor
+        <div className={styles.toolbar}>
+          <button
+            className={
+              editor?.isActive('bold')
+                ? styles.toolbarButtonActive
+                : styles.toolbarButton
+            }
+            onClick={toggleBold}
+          >
+            Bold
+          </button>
+          <button
+            className={
+              editor?.isActive('italic')
+                ? styles.toolbarButtonActive
+                : styles.toolbarButton
+            }
+            onClick={toggleItalic}
+          >
+            Italic
+          </button>
+        </div>
+        <EditorContent editor={editor} className={styles.textEditorContent} />
       </div>
     </div>
   )
