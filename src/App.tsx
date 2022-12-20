92c169f66454513522e5c9c00a58017b73c3a200
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-12-20 18:24:14 +0100

    style: temporary layout setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import styles from './App.module.css'
+
 function App() {
-  return <></>
+  return (
+    <div className={styles.pageContainer}>
+      <div className={styles.sidebar}>Sidebar</div>
+      <div className={styles.editorContainer}>
+        <div className={styles.toolbar}>Toolbar</div>
+        Text Editor
+      </div>
+    </div>
+  )
 }
 
 export default App
